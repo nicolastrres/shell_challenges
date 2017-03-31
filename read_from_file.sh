faa152ead375bd316e3cc41a577cbf0e540177b8
Nicolas Agustin Torres <nicolastrres@gmail.com>
2017-03-30 21:57:29 -0300

    Creates script to read files

---
+++ read_from_file.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Read a file. If the first parameter is a file then print its content otherwise read a default file. default_file.txt
+
+. ./print.sh
+
+
+if [ -z ${1} ]
+then
+    in_yellow "Usage: ./read_file.sh FILE_NAME"
+    in_yellow "If the first argument is a file then it will print its content otherwise it will print the content of default_file.txt"
+else
+    if [ -f ${1} ];then
+        cat ${1}
+    else
+        cat default_file.txt
+    fi
+fi
