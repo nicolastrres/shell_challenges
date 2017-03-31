e17d723f210f43297058dc644723519e56a07f58
Nicolas Agustin Torres <nicolastrres@gmail.com>
2017-03-30 21:33:26 -0300

    Creates print script to put echo function with colours

--- create_file.sh
+++ create_file.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-function in_red()     { echo -e "\033[31m${1}\033[0m"; }
-function in_green()   { echo -e "\033[32m${1}\033[0m"; }
-function in_cyan()    { echo -e "\033[36m${1}\033[0m"; }
-function in_yellow()  { echo -e "\033[33m${1}\033[0m"; }
-
+. ./print.sh
 
 function verify_file_existence() {
     local file_name=${1}
